7be98caa4f681aa570269b77941ae9a11c265d3a
Steph Dietz <stephdietz@Stephs-MBP.lan>
2024-07-25 12:57:10 -0500

    add italics

--- app/components/llm-response-box.tsx
+++ app/components/llm-response-box.tsx
@@ -4,7 +4,7 @@ export default async function LLMResponse({query}: {query: string | null}) {
     const response = await generateSearchObjectFromLLM(query || "")
     const userResponse = response?.data?.generateSearchObjectFromLLM?.userResponse
     return (
-        <div className="text-white text-xs bg-indigo-500 p-2 rounded">
+        <div className="text-white text-xs bg-indigo-500 p-2 rounded italic">
             {userResponse}
         </div>
     )
